5214a1b22d78ac0d9e04158380ee036a065e8b71
Malelus <matin1608@gmail.com>
2021-02-02 17:19:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Strona o szczepionkach na COVID-19
+
+> ### [Link do strony](https://malelus.github.io/SzczepionkiCovid/)
+
+---
+
+#### Strona została napisana w edytorze Visual Studio Code używając preprocesora SASS do pisania kodu CSS oraz użyto ikon ze strony https://fontawesome.com/.
+
+- czcionki:
+  - [Roboto](https://fonts.google.com/specimen/Roboto?query=robot),
+  - [Bebas Neue](https://fonts.google.com/specimen/Bebas+Neue?query=bebas).
+
+---
+
+### Obowiązkowe:
+
+- układ:
+
+  - logo,
+  - linki,
+  - zawartość,
+  - stopka.
+
+- 3 szczepionki (Moderna, AstraZeneca, CureVac) ([źródło](https://businessinsider.com.pl/technologie/nowe-technologie/szczepionki-przeciw-covid-19-moderna-astrazeneca-curevac-sanofi/k4znpqf)),
+
+- formularz zapisu na szczepienie.
+
+---
+
+### Dodatkowe:
+
+#### Budowa:
+
+- strona zbudowana została responsywnie dzięki czemu umożliwia swobodne i wygodne korzystanie z niej na każdym urządzeniu:
+
+| Mobile                                                     | Tablet                                                                                        | Desktop                                                             |
+| ---------------------------------------------------------- | --------------------------------------------------------------------------------------------- | ------------------------------------------------------------------- |
+| <699px                                                     | 700px - 999px                                                                                 | >1000px                                                             |
+| widok mobilny, hamburger menu (pierwotne ułożenie strony), | widok tabletu, pasek nawigacji na górze, dostosowanie czcionek i animacji do rozmiaru ekranu, | widok desktop, dostosowanie czcionek i animacji do rozmiaru ekranu, |
+
+- użyto znaczników semantycznych,
+
+- okno popup umieszczone w stopce strony, zawiera:
+
+  - skrócone informacje o zawartości strony (dodatkowy link do formularza szczepień),
+  - link do repozytorium GitHub'a,
+  - informacje o autorze,
+  - datę wykonania,
+
+  - okno można wyłączyć na 3 sposoby:
+
+    - przycisk Zamknij,
+    - kliknięcie poza obszar okna,
+    - za pomocą przycisku ESC.
+
+#### Wygląd:
+
+- animacja przejścia między podstronami wykonana przy pomocy CSS oraz JavaScript,
+
+- linki aktywne oraz znacznik na której stronie aktualnie znajduję się użytkownik,
+
+- zmieniony wygląd scrollbar'a,
+
+- animacja na głównej stronie, po najechaniu na nazwę akcji szczepień pojawia się link przenoszący na stronę [gov](https://www.gov.pl/web/szczepimysie),
+
+- link do strony głównej umieszczony w napisie "SzczepionkiCovid",
+
+- symulowane wysłanie formularza na serwer komunikowany poprzez komunikat, następnie strona zostaje odświeżona.
+
+#### Dodatki:
+
+- informacje w konsoli o statusie anmiacji oraz o czasach trwania wszystkich animacji znajdujących się na stronie,
+
+- możliwość wyłączenia całkowicie animacji znajdujących się na stronie.
+
+  > (aby wyłączyć animacje: Ustawienia > Ułatwienia dostępu > Wyświetlacz > Pokaż animacje w systemie Windows (Wyłącz)) (Ustawienie znajduje się w pliku \_reset.scss).
+
+---
+
+#### Wykonał: Mateusz Narowski
+
+#### Data: 02.02.2021
---
+++ addons/main.js
@@ -0,0 +1,181 @@
+window.onload = () => {
+  //===========================//
+  //===// Loading classes //===//
+  //===========================//
+
+  // - hamburger
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.nav');
+
+  // - page transition
+  const transitionElement = document.querySelector('.transition');
+  const anchors = document.querySelectorAll('a:not(.external)');
+
+  // - popup
+  const popupBtn = document.querySelectorAll('.show-popup');
+  const popup = document.querySelector('.popup');
+
+  // - form
+  const form = document.getElementById('form');
+  const formResult = document.querySelector('.form__result');
+
+  //=============================//
+  //===// Loading variables //===//
+  //=============================//
+
+  // Defining const for motion status check
+  const motionCheck = window.matchMedia('(prefers-reduced-motion: reduce)');
+
+  // Defining variables array
+  const durationsArray = [];
+  const varArray = [
+    '--transition-time',
+    '--btn-anim-time',
+    '--popup-time',
+    '--logo-anim-time',
+    '--nav-hide-time',
+    '--nav-show-time',
+    '--main-anim-time',
+    '--form-result-visibility',
+    '--anim-delay',
+    '--anim-delay--logo-icon',
+  ];
+
+  // Defining const for global variables source and quantity of variable array
+  const varGlobal = getComputedStyle(document.body);
+  const varQuantity = varArray.length;
+
+  // Inserting all time durations into durations array
+  for (let i = 0; i < varQuantity; i++) {
+    durationsArray[i] = varGlobal.getPropertyValue(varArray[i]);
+  }
+
+  //================================//
+  //====// Variables formating //===//
+  //================================//
+
+  // Subtracting `s` letter from loaded string
+  for (let i = 0; i < varQuantity; i++) {
+    durationsArray[i] = durationsArray[i].substring(0, durationsArray[i].length - 1);
+  }
+
+  // Setting own variable for transition time
+  const timeTransition = durationsArray[0] * 1000;
+  const timeFormResult = durationsArray[7] * 1000;
+
+  // Setting style for console timing info
+  for (let i = 0; i < varQuantity; i++) {
+    if (durationsArray[i].indexOf('.') < '') durationsArray[i] = durationsArray[i] + '.0';
+  }
+
+  //===================================//
+  //===// Reduced motion - Status //===//
+  //===================================//
+
+  motionCheck.addEventListener('change', consoleInit);
+
+  function consoleInit() {
+    if (!motionCheck || motionCheck.matches) {
+      motionStatus = `OFF`;
+      console.clear();
+      consoleLog();
+    } else {
+      motionStatus = `ON`;
+      console.clear();
+      consoleLog();
+    }
+  }
+
+  //====================//
+  //====// Console //===//
+  //====================//
+
+  // Console log animation status & transitions durations & delays
+
+  consoleInit();
+
+  function consoleLog() {
+    console.log(`# Scripts were loaded correctly! (Time is given in seconds)`);
+    console.log(`# Animations are ` + motionStatus + `\n\n`);
+    console.log(`# Page transition:`);
+    console.log(`  - static screen: ` + durationsArray[0]);
+    console.log(`  - transition: ` + durationsArray[0]);
+    console.log(`# Button animation time: ` + durationsArray[1]);
+    console.log(`# Popup time: ` + durationsArray[2]);
+    console.log(`# Logo animation time: ` + durationsArray[3]);
+    console.log(`# Navigation:`);
+    console.log(`  - hide: ` + durationsArray[4]);
+    console.log(`  - show: ` + durationsArray[5]);
+    console.log(`# Main animation: ` + durationsArray[6]);
+    console.log(`# Form result visibility: ` + durationsArray[7]);
+    console.log(`# Delays:`);
+    console.log(`  - default: ` + durationsArray[8]);
+    console.log(`  - logo icon: ` + durationsArray[9]);
+  }
+
+  //====================//
+  //===// Scripts //===//
+  //===================//
+
+  //===// Hamburger //===//
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('nav--visibility');
+  });
+
+  //===// Page transition //===//
+  setTimeout(() => {
+    transitionElement.classList.remove('transition--active');
+  }, timeTransition);
+
+  anchors.forEach((anchors) => {
+    const anchor = anchors;
+
+    anchor.addEventListener('click', (e) => {
+      e.preventDefault();
+      let target = e.target.href;
+
+      transitionElement.classList.add('transition--active');
+
+      setTimeout(() => {
+        window.location.href = target;
+      }, timeTransition);
+    });
+  });
+
+  //===// Popup //===//
+
+  //===// Popup show & close with button //===//
+  popupBtn.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      const popupTarget = e.target.dataset.target;
+
+      const popupElement = document.querySelector(popupTarget);
+      if (popupElement != null) {
+        popupElement.classList.toggle('popup--active');
+      }
+    });
+  });
+
+  //===// Popup close on escape key press //===//
+  document.addEventListener('keydown', esc);
+  function esc(e) {
+    if (e.which === 27) {
+      popup.classList.remove('popup--active');
+    }
+  }
+
+  //===// Form //===//
+
+  document.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    form.reset();
+    formResult.classList.add('form__result--true');
+
+    setTimeout(() => {
+      location.reload();
+    }, timeFormResult + 125);
+  });
+};
